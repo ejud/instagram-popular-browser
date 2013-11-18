4831c4ffd33498cf1b5a38795f77a4cf50aadc69
Ethan Jud <ethanpjud@gmail.com>
2013-11-18 14:59:06 -0800

    Initial Commit

---
+++ Instagram Photo Browser/IPBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPBAppDelegate.h
+//  Instagram Photo Browser
+//
+//  Created by Ethan Jud on 11/18/13.
+//  Copyright (c) 2013 Ethan Jud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Instagram Photo Browser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Instagram Photo Browser
+//
+//  Created by Ethan Jud on 11/18/13.
+//  Copyright (c) 2013 Ethan Jud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IPBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IPBAppDelegate class]));
+    }
+}
